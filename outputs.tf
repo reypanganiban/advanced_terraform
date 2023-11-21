a59238c49d848264cb4a13e05f2fcab73224cb05
reypanganiban <reynaldo.panganiban.jr@gmail.com>
2023-11-21 05:52:29 +0000

    loop using count

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,11 @@
-output "subnet-gateway" {
-  value = google_compute_subnetwork.subnet-1.gateway_address
-}
-
 output "nginx-public-ip" {
   value = google_compute_instance.nginx_instance.network_interface[0].access_config[0].nat_ip
+}
+
+output "web-instances" {
+  value = google_compute_instance.web-instances[*].network_interface[0].network_ip
+}
+
+output "db-private-ip" {
+  value = google_compute_instance.mysqldb.network_interface[0].network_ip
 }
